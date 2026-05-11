4803738dbd336ffb5996a6375d299fe7d74e4ee0
Kelvin <kellyvin868@gmail.com>
2026-05-11 18:54:23 +0300

    delete vercel

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -131,7 +131,7 @@ const Bookings = ({token}) => {
 
   ):(
     <div  className='w-full opacity-0 text-2xl text-center mt-30'>
-      <h2 className='text-2xl font-semibold'>No bookings at the moment,please make some bookings</h2>
+      <h2 className='text-2xl font-semibold'>No booking at the moment,please make some bookings</h2>
 
     </div>
   )
